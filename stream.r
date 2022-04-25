6752faffe7f9c8e328c107b1f3b906b9b2a23506
Agata Żywot <agata.zywot@gmail.com>
2022-04-25 22:44:18 +0200

    Stream chart

--- stream.r
+++ stream.r
@@ -3,6 +3,8 @@ library(palmerpenguins)
 library(tidyverse)
 library(scales)
 library(showtext)
+library(readxl)
+library(countrycode)
 
 wdi <- read_excel("resources/Data pack/World_Development_Indicators.xlsx")
 wdi <- data.frame(wdi)
@@ -10,8 +12,9 @@ wdi <- data.frame(wdi)
 wdi <- wdi %>% mutate_all(funs(replace(., .== "..", NA))) %>%
   filter(Series.Name == "Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)" | 
            Series.Name == "GDP per capita (current US$)"|
-           Series.Name == "Suicide mortality rate (per 100,000 population)" |
-           Series.Name == "Life expectancy at birth, total (years)")
+           Series.Name == "Unemployment, total (% of total labor force) (national estimate)" |
+           Series.Name == "Life expectancy at birth, total (years)"|
+           Series.Name == "CO2 emissions (kt)")
 wdi <- wdi[colSums(!is.na(wdi)) > 0]
 wdi <- subset(wdi, select=-c(Series.Code, Country.Code))
   
@@ -24,19 +27,23 @@ wdi <- mutate(wdi, Mean.Val = rowMeans(select(wdi, starts_with("X")), na.rm = TR
   spread(key=Series.Name, value=Mean.Val) %>%
   subset(select=-Country.Name)
 
-wdi <- rename(wdi, c(GDP ="GDP per capita (current US$)", 
-                  Life.Exp = "Life expectancy at birth, total (years)", 
-                  Suicide = "Suicide mortality rate (per 100,000 population)",
-                  Alcohol = "Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)"))
+wdi <- rename(wdi, c(
+                  Alcohol = "Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)",
+                  GDP ="GDP per capita (current US$)", 
+                  Unemployment = "Unemployment, total (% of total labor force) (national estimate)",
+                  Life.Exp = "Life expectancy at birth, total (years)",
+                  CO2 = "CO2 emissions (kt)"
+                  ))
 
 wdi_grouped <- wdi %>%
   drop_na() %>%
   group_by(continent) %>%
   summarise(
     GDP = mean(GDP, na.rm=TRUE),
-    'Life\nexpectancy' = mean(Life.Exp, na.rm=TRUE),
-    'Suicide rate' = mean(Suicide, na.rm=TRUE),
-    'Alco\nconsumption' = mean(Alcohol, na.rm=TRUE)
+    'Life expectancy' = mean(Life.Exp, na.rm=TRUE),
+    'Unemployment rate' = mean(Unemployment, na.rm=TRUE),
+    'Alcohol consumption' = mean(Alcohol, na.rm=TRUE),
+    CO2 = mean(CO2, na.rm=TRUE)
   ) %>%
   ungroup() %>%
   mutate_at(vars(-continent), rescale)
@@ -45,8 +52,13 @@ wdi_grouped <- wdi %>%
 # https://r-graph-gallery.com/web-radar-chart-with-R.html
 plt <- wdi_grouped %>%
   ggradar(
+    plot.title = "Comparison of the socio-economic factors of continents",
+    legend.title = "Continents:",
     grid.label.size = 5,  # Affects the grid annotations (0%, 50%, etc.)
     axis.label.size = 5, # Afftects the names of the variables
-    group.point.size = 3   # Simply the size of the point 
+    group.point.size = 3,
+    background.circle.transparency = 0
   )
 plt
+
+?ggradar
